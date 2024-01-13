02be429a5b9126e57b582e1e0e4956851c1c94eb
harshit7yadav <harshit.yadav8284@gmail.com>
2024-01-13 21:06:19 +0530

    user input

---
+++ userdef.py
@@ -0,0 +1,16 @@
+#furthermore moving ahead with the topicd yet we learned about the different printing statements which carries the value within themselves now lets learn how to take arbitary values to the variables at run-time i.e during the execution of the program.
+#input() :- input() is the function used in python programming to take the values as per the user hence the user defined value, as the values are defined the user itself.
+
+#input1
+var=input()
+print(var)
+
+#lets check what happens if the value is passed to the input() function
+#input2
+var= input("Enter the string: ")
+print("this is","",var)
+
+#input3
+print("Tell me it's funny")
+var=input()
+print("its",var,"Really funny")
