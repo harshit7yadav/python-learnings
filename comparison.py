b80f42ec65969df1de78aa5a24f20a6a5a3bde26
harshit7yadav <harshit.yadav8284@gmail.com>
2024-01-20 11:45:21 +0530

    decision making operators

---
+++ comparison.py
@@ -0,0 +1,25 @@
+#taking about the decision making operators in python programming :- 
+#NOTE:-gives the output of the program in form of boolean i.e true or false.
+
+
+#1.equality operator(==):= this operator checks the equality of the two value.
+var1=1
+var2=2
+print(var1==var2)
+
+#2.Inequality operator(!=):- the inequality operator checks the inequality of the two values.
+var=1
+var1=1
+print(var!=1)
+print(var1==1)
+print(var==var1)
+print(var!=var1)
+
+#3.grater than and less than(>,<), grater than equal to and grater than equal to(<=):-the operator checks is the value is grater than the other or not.
+var1=input("Enter the value1: ")
+var2=input("Enter the value1: ")
+print(var1>var2)
+print(var1<var2)
+print(var1<=var2)
+print(var1>=var2)
+
